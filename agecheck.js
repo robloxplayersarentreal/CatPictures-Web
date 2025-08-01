0f8c880329106c30800515183ddcdeb98de51c9e
robloxplayersarentreal <152041187+robloxplayersarentreal@users.noreply.github.com>
2025-08-01 00:20:08 -0400

    Create agecheck.js

---
+++ agecheck.js
@@ -0,0 +1,14 @@
+function agecheck() {
+
+ age = prompt("How old are you: ")
+if (age > 12){
+alert("Welcome")
+sessionStorage.setItem("Age", age);
+}
+else {
+  alert("you are way too young")
+window.location.assign("google.com")
+}
+
+
+}
