6eb2fc7b0c74e90bfeb5ea48abb4a0a22dc9ed75
robloxplayersarentreal <152041187+robloxplayersarentreal@users.noreply.github.com>
2025-08-01 11:16:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-//function for checking if they like dogs
 console.log("What do you think you're doing");
+
 window.onload = function() {
-dogcheck();
-}
+  dogcheck();
+};
+
 function dogcheck() {
+  const dogs = ["Dogs", "Dog", "dog", "dogs", "DOGS", "DOG"];
+  const dogAnswer = prompt("Dogs or Cats");
 
- const dogs = ["Dogs","Dog","dog","dogs","DOGS","DOG"];
- dog? = prompt("Dogs or Cats");
-if (dog? = dogs){
-alert("NO DOG PEOPLE ALLOWED");
-window.location.assign("https://www.thesprucepets.com/why-cats-are-better-than-dogs-554880");
-}
-else {
-  alert("Welcome");
-}
-
-}
+  if (dogs.includes(dogAnswer)) {
+    alert("NO DOG PEOPLE ALLOWED");
+    window.location.assign("https://www.thesprucepets.com/why-cats-are-better-than-dogs-554880");
+  } else {
+    alert("Welcome");
+  }
 }
