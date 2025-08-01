7583337c909d29c786dc311b753b989475bdc43e
robloxplayersarentreal <152041187+robloxplayersarentreal@users.noreply.github.com>
2025-08-01 11:10:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 //function for checking if they like dogs
-window.onlaod = function agecheck() {
+console.log("What do you think you're doing");
+window.onload = function() {
+dogcheck();
+}
+function dogcheck() {
 
- const dogs = ["Dogs","Dog","dog","dogs","DOGS","DOG"]
- dog? = prompt("Dogs or Cats")
+ const dogs = ["Dogs","Dog","dog","dogs","DOGS","DOG"];
+ dog? = prompt("Dogs or Cats");
 if (dog? = dogs){
-alert("NO DOG PEOPLE ALLOWED")
-window.location.assign("https://www.thesprucepets.com/why-cats-are-better-than-dogs-554880")
+alert("NO DOG PEOPLE ALLOWED");
+window.location.assign("https://www.thesprucepets.com/why-cats-are-better-than-dogs-554880");
 }
 else {
-  alert("Welcome")
+  alert("Welcome");
 }
 
 }
